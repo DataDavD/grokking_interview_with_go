11a415443bdc6589fbfc63bcd68a368f9c1e0756
David Dansby <39511285+DataDavD@users.noreply.github.com>
2023-05-23 23:59:26 -0700

    Add tests for reverse words

---
+++ two_pointers/reverse_words_test.go
@@ -0,0 +1,40 @@
+package palindrome
+
+import "testing"
+
+func Test_reverseWords(t *testing.T) {
+	tests := []struct {
+		name    string
+		testStr string
+		want    string
+	}{
+		{
+			name:    "valid palindrome 1",
+			testStr: "Hello World",
+			want:    "World Hello",
+		},
+		{
+			name:    "valid palindrome 2",
+			testStr: "We love Golang",
+			want:    "Golang love We",
+		},
+		{
+			name:    "valid palindrome 3",
+			testStr: "Hello World",
+			want:    "World Hello",
+		},
+		{
+			name:    "valid palindrome 4",
+			testStr: "To be or not to be",
+			want:    "be to not or be To",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseWords(tt.testStr); got != tt.want {
+				t.Errorf("reverseWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
